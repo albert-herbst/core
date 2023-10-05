b13a0aa2536ee179fbdcaab6c8b77317f8ec883c
Adam <adsv18@student.bth.se>
2023-10-05 22:40:27 +0000

    Remove maintainability code-smell

--- homeassistant/components/spider/__init__.py
+++ homeassistant/components/spider/__init__.py
@@ -37,17 +37,15 @@ CONFIG_SCHEMA = vol.Schema(
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up a config entry."""
     hass.data[DOMAIN] = {}
-    if DOMAIN not in config:
-        return True
+    if DOMAIN in config:
+        conf = config[DOMAIN]
 
-    conf = config[DOMAIN]
-
-    if not hass.config_entries.async_entries(DOMAIN):
-        hass.async_create_task(
-            hass.config_entries.flow.async_init(
-                DOMAIN, context={"source": SOURCE_IMPORT}, data=conf
+        if not hass.config_entries.async_entries(DOMAIN):
+            hass.async_create_task(
+                hass.config_entries.flow.async_init(
+                    DOMAIN, context={"source": SOURCE_IMPORT}, data=conf
+                )
             )
-        )
 
     return True
 
