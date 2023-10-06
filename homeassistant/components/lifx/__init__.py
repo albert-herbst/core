e85a13958d0d62fbe45b789edf50062a614ce6bb
Adam Alrefai <adamalrefai9805@gmail.com>
2023-10-06 15:33:52 +0000

    Fix invariant returns in async_setup_entry

--- homeassistant/components/lifx/__init__.py
+++ homeassistant/components/lifx/__init__.py
@@ -188,48 +188,46 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up LIFX from a config entry."""
-    if async_entry_is_legacy(entry):
-        return True
+    if not async_entry_is_legacy(entry):
+        if legacy_entry := async_get_legacy_entry(hass):
+            # If the legacy entry still exists, harvest the entities
+            # that are moving to this config entry.
+            async_migrate_entities_devices(hass, legacy_entry.entry_id, entry)
 
-    if legacy_entry := async_get_legacy_entry(hass):
-        # If the legacy entry still exists, harvest the entities
-        # that are moving to this config entry.
-        async_migrate_entities_devices(hass, legacy_entry.entry_id, entry)
+        assert entry.unique_id is not None
+        domain_data = hass.data[DOMAIN]
+        if DATA_LIFX_MANAGER not in domain_data:
+            manager = LIFXManager(hass)
+            domain_data[DATA_LIFX_MANAGER] = manager
+            manager.async_setup()
 
-    assert entry.unique_id is not None
-    domain_data = hass.data[DOMAIN]
-    if DATA_LIFX_MANAGER not in domain_data:
-        manager = LIFXManager(hass)
-        domain_data[DATA_LIFX_MANAGER] = manager
-        manager.async_setup()
+        host = entry.data[CONF_HOST]
+        connection = LIFXConnection(host, TARGET_ANY)
+        try:
+            await connection.async_setup()
+        except socket.gaierror as ex:
+            connection.async_stop()
+            raise ConfigEntryNotReady(f"Could not resolve {host}: {ex}") from ex
+        coordinator = LIFXUpdateCoordinator(hass, connection, entry.title)
+        coordinator.async_setup()
+        try:
+            await coordinator.async_config_entry_first_refresh()
+        except ConfigEntryNotReady:
+            connection.async_stop()
+            raise
 
-    host = entry.data[CONF_HOST]
-    connection = LIFXConnection(host, TARGET_ANY)
-    try:
-        await connection.async_setup()
-    except socket.gaierror as ex:
-        connection.async_stop()
-        raise ConfigEntryNotReady(f"Could not resolve {host}: {ex}") from ex
-    coordinator = LIFXUpdateCoordinator(hass, connection, entry.title)
-    coordinator.async_setup()
-    try:
-        await coordinator.async_config_entry_first_refresh()
-    except ConfigEntryNotReady:
-        connection.async_stop()
-        raise
-
-    serial = formatted_serial(coordinator.serial_number)
-    if serial != entry.unique_id:
-        # If the serial number of the device does not match the unique_id
-        # of the config entry, it likely means the DHCP lease has expired
-        # and the device has been assigned a new IP address. We need to
-        # wait for the next discovery to find the device at its new address
-        # and update the config entry so we do not mix up devices.
-        raise ConfigEntryNotReady(
-            f"Unexpected device found at {host}; expected {entry.unique_id}, found {serial}"
-        )
-    domain_data[entry.entry_id] = coordinator
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+        serial = formatted_serial(coordinator.serial_number)
+        if serial != entry.unique_id:
+            # If the serial number of the device does not match the unique_id
+            # of the config entry, it likely means the DHCP lease has expired
+            # and the device has been assigned a new IP address. We need to
+            # wait for the next discovery to find the device at its new address
+            # and update the config entry so we do not mix up devices.
+            raise ConfigEntryNotReady(
+                f"Unexpected device found at {host}; expected {entry.unique_id}, found {serial}"
+            )
+        domain_data[entry.entry_id] = coordinator
+        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
